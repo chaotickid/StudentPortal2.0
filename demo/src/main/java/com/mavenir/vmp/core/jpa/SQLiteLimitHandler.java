195a3f0b19bda4ee1e6df29db89afc08dc52e2f2
Aditya Patil <aditya.patil@mavenir.com>
2024-12-13 05:59:15 +0530

    --SPRING BOOT WORKING-- - fixed issue for SQLiteLimitHandler.java need to review

--- demo/src/main/java/com/mavenir/vmp/core/jpa/SQLiteLimitHandler.java
+++ demo/src/main/java/com/mavenir/vmp/core/jpa/SQLiteLimitHandler.java
@@ -1,34 +1,56 @@
-///**
-// *
-// */
-//package com.mavenir.vmp.core.jpa;
-//
-//import org.hibernate.dialect.Dialect;
-////import org.hibernate.dialect.pagination.LegacyLimitHandler;
-////import org.hibernate.engine.spi.RowSelection;
-//
-///**
-// * Legacy Limit handler does not override bindLimitParametersInReverseOrder
-// * returning false and breaking paging along with it.
-// *
-// */
-//public class SQLiteLimitHandler /*extends LegacyLimitHandler*/ {
-//
-//	/**
-//	 * @param dialect
-//	 * @param sql
-//	 * @param selection
-//	 */
-////	public SQLiteLimitHandler(Dialect dialect, String sql, RowSelection selection) {
-////		super(dialect);
-////	}
-//
-//	/* (non-Javadoc)
-//	 * @see org.hibernate.dialect.pagination.LegacyLimitHandler#bindLimitParametersInReverseOrder()
-//	 */
-//	//@Override
-//	public boolean bindLimitParametersInReverseOrder() {
-//		return true;
-//	}
-//
-//}
+package com.mavenir.vmp.core.jpa;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.pagination.LimitHandler;
+import org.hibernate.query.spi.Limit;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Custom Limit Handler for SQLite to handle limit/offset pagination.
+ */
+public class SQLiteLimitHandler implements LimitHandler {
+
+    private final Dialect dialect;
+
+    public SQLiteLimitHandler(Dialect dialect) {
+        this.dialect = dialect;
+    }
+
+
+    @Override
+    public boolean supportsLimit() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsOffset() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsLimitOffset() {
+        return false;
+    }
+
+    @Override
+    public String processSql(String s, Limit limit) {
+        return "";
+    }
+
+    @Override
+    public int bindLimitParametersAtStartOfQuery(Limit limit, PreparedStatement preparedStatement, int i) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int bindLimitParametersAtEndOfQuery(Limit limit, PreparedStatement preparedStatement, int i) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public void setMaxRows(Limit limit, PreparedStatement preparedStatement) throws SQLException {
+
+    }
+}
